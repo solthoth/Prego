98d06a5ae251167e5bcf96498f3baa0b5ef2cc23
Carlos Cesar Barajas Jr <soldotsol@gmail.com>
2016-09-29 22:28:23 -0700

    Refactor configurations for website settings

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('dotenv').config();
+
+// Setup new routes here
 var routes = require('./routes/index');
 
 var app = express();
---
+++ settings.js
@@ -0,0 +1,14 @@
+var app = require('./app');
+var compression = require('compression');
+
+// Set global settings
+app.set('case sensitive routing', false);
+
+// Set environment specific settings
+if(app.get('env') === 'development') {
+    console.log('Running['+app.get('env')+'] on port ' + process.env.PORT);
+    app.locals.pretty = true;
+} else { // assume production
+    app.locals.pretty = false;
+    app.use(compression);
+}
