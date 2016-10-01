28dc2141469a4c40976a4cd71fbad4134ace9d68
Carlos Cesar Barajas Jr <soldotsol@gmail.com>
2016-09-30 21:59:23 -0700

    Integrate sass and expand gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 var gulp = require('gulp');
 var jslint = require('gulp-jslint');
+var sass = require('gulp-sass');
+var del = require('del');
 
 var paths = {
+    clean: {
+        css: './public/stylesheets/**/*.css'
+    },
+    scss: {
+        input: "./styleguide/**/*.scss",
+        output: "./public/stylesheets"
+    },
     jslint: [
-        'app.js',
-        'settings.js',
-        'routes/**/*.js',
-        'public/javascripts/**/*.js'
+        './app.js',
+        './settings.js',
+        './routes/**/*.js',
+        './public/javascripts/**/*.js'
     ]
 };
+
+gulp.task('clean:css', function(){
+    return del(paths.clean.css);
+});
+
+gulp.task('build:css', function(){
+    var outputStyle = "compressed";
+    if(process.env.NODE_ENV === 'development') {
+        outputStyle = "nested";
+    }
+    return gulp.src(paths.scss.input)
+        .pipe(sass({
+            outputStyle: outputStyle
+        }).on('error', sass.logError))
+        .pipe(gulp.dest(paths.scss.output));
+});
 
 gulp.task('jslint', function(){
     return gulp.src(paths.jslint)
@@ -23,11 +48,15 @@ gulp.task('jslint', function(){
         .pipe(jslint.reporter('stylish'));
 });
 
-gulp.task('test', ['jslint'], function(){
+gulp.task('clean', ['clean:css'], function(){
+
+});
+
+gulp.task('test', function(){
     
 });
 
-gulp.task('build', ['test'], function() {
+gulp.task('build', ['jslint', 'build:css'], function() {
 
 });
 
